5bfc950afccdaf1c455a6b81af18a25dab055dee
ismailamca <adamjohhanson@gmail.com>
2014-10-26 11:05:22 +0200

    converter

--- broken_sweep.sh
+++ broken_sweep.sh
@@ -7,18 +7,18 @@ cowsay "this is the fetch check for new slides please paste your slidefiles to t
 export LC_ALL=C
 # these segments searchs .ppts and .pptxs in slides folder
 # then rename with a random name and covert to required formats that we are in need to
-for file in $(find -type f -name '*.ppt');
+for file in $(find slides/ -type f -name '*.ppt');
 do
 new_name=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
 mv $file $new_name.ppt
-libreoffice --headless --invisible --convert-to pptx $new_name.ppt
+libreoffice --headless --invisible --convert-to pptx $new_name.pptx
 done
 
 cowsay -s "I've done the pptx conversions. Now strating fetch conversions "
 
 # now starting to converting into fetch things
 # this'll route the pptxs to the fetch_converter.py and convert them into 
-for pptxs in $(find -type f -name '*.pptx');
+for pptxs in $(find slides/ -type f -name '*.pptx');
 do
-python fetch_converter.py $pptxs
+python slides/fetch_converter.py $pptxs
 done
