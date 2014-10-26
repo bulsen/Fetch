dec58087eb628fe0cc51ea88c08cde3376ecb2df
ismailamca <adamjohhanson@gmail.com>
2014-10-26 11:21:10 +0200

    install

--- install_script.sh
+++ install_script.sh
@@ -3,8 +3,8 @@
 export LC_ALL=C
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y
 sudo apt-get install build-essential -y
-sudo apt-get install cowsay mongodb python-dev python-pip pdf2svg libxml2-dev libxslt1-dev python-lxml libtiff4-dev libjpeg8-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev tcl8.5-dev tk8.5-dev python-tk
-sudo pip install python-pptx pillow pymongo
+sudo apt-get install cowsay mongodb python-dev python-pip pdf2svg libxml2-dev libxslt1-dev python-lxml libtiff4-dev libjpeg8-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev tcl8.5-dev tk8.5-dev python-tk libreoffice
+sudo pip install pillow python-pptx pymongo
 
 cowsay "that should work! now install your slides to the slides/ directory and convert them with sweep.sh 
 
